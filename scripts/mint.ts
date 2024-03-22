0c9d3ca10c763450fc7c370df964748f77703662
Bernardo Cardoso <29538426+bmastc@users.noreply.github.com>
2024-03-22 19:00:48 +0000

    Update geth config to London fork. Add more default accounts and mint wXX to all

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,9 +1,9 @@
 import { ethers } from "hardhat";
 import WrappedXXArtifact from "../wrappedxx/artifacts/contracts/WrappedXX.sol/WrappedXX.json";
 import { WrappedXX } from "../typechain-types";
+import { ContractTransactionResponse } from "ethers";
 
 const address = "0x3f709398808af36ADBA86ACC617FeB7F5B7B193E";
-const destination = "0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7"; // Dave
 const amount = 100000000000; // 100 wXX
 
 async function main() {
@@ -14,11 +14,18 @@ async function main() {
   const contract = await ethers.getContractAt(WrappedXXArtifact.abi, address);
   const wrappedXX = contract as unknown as WrappedXX;
 
-  const tx = await wrappedXX.connect(sender).mint(destination, amount);
-  console.log(
-    `Minting ${amount / 1e9} wXX to ${destination} in transaction ${tx.hash}`
-  );
-  await tx.wait(1);
+  let tx: ContractTransactionResponse | undefined; // Initialize tx variable
+  for (const destination of signers) {
+    tx = await wrappedXX.connect(sender).mint(destination.address, amount);
+    console.log(
+      `Minting ${amount / 1e9} wXX to ${destination.address} in transaction ${
+        tx.hash
+      }`
+    );
+  }
+  if (tx) {
+    await tx.wait(1);
+  }
 }
 
 main()
